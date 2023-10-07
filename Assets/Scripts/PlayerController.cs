0214636ee4a88527e522bcffc60bff058ddec806
Mateus Alves <miatempo13@gmail.com>
2023-10-07 16:05:26 -0300

    Added boost and slowdown functionality to PlayerController.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+    private float baseSpeed;
+    [SerializeField] private float accelerateSpeed;
     [SerializeField] private float torqueAmount = 1f;
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+        baseSpeed = surfaceEffector2D.speed;
     }
-    void Update() {
-        if(Input.GetKey(KeyCode.A)) {
+    void Update()
+    {
+        AcceleratePlayer();
+        RotatePlayer();
+    }
+    private void AcceleratePlayer()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            surfaceEffector2D.speed = baseSpeed + accelerateSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            surfaceEffector2D.speed = baseSpeed - accelerateSpeed;
+        }
+        else {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.A)) {
             rb2d.AddTorque(torqueAmount);
         }
-        else if(Input.GetKey(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D)) {
             rb2d.AddTorque(-torqueAmount);
         }
     }
