1de9a16d956ecad77c8939c316ff118fdb51b561
Mateus Alves <miatempo13@gmail.com>
2023-10-07 14:12:08 -0300

    Added SceneManagers for the finish line and crashes.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour {
+    [SerializeField] private float timeToResetScene = 1.5f;
     private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Crash!");
+        Invoke("ReloadScene", timeToResetScene);
+    }
+    public void ReloadScene() {
+        SceneManager.LoadScene("Level1");
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour {
+    [SerializeField] private float timeToResetScene = 1.5f;
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player")
-        {
-            Debug.Log("Finished.");
+        if (other.tag == "Player") {
+            Invoke("ReloadScene", timeToResetScene);
         }
+    }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene("Level1");
     }
 }
